a39e825351ca7c6223937d942a1090dfa1c9b550
Gio d'Amelio <giodamelio@gmail.com>
2014-10-27 22:48:09 -0700

    Express hello world

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
-console.log("Hello world");
+var express = require("express");
+
+var app = express();
+
+app.get("/", function(req, res) {
+    res.send("Hello World");
+});
+
+app.listen(3141);
+console.log("App is running at http://localhost:3141");
 
