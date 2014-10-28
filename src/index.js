f3e87069b6b9c62c688913dfbd03573cce36bbd7
Gio d'Amelio <giodamelio@gmail.com>
2014-10-27 23:14:54 -0700

    Send home file

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
+var fs = require("fs");
+var path = require("path");
+
 var express = require("express");
 
 var app = express();
 
+// Serve homepage
 app.get("/", function(req, res) {
-    res.send("Hello World");
+    res.sendFile(path.resolve(__dirname, "../home.html"));
 });
 
 app.listen(3141);
